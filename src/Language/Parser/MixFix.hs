90ac0002a81fcd4ec11ca1b1f65af4c0d74bac8f
Rob Rix <rob.rix@github.com>
2016-01-12 21:43:40 -0500

    Operator.

--- src/Language/Parser/MixFix.hs
+++ src/Language/Parser/MixFix.hs
@@ -1,7 +1,12 @@
 module Language.Parser.MixFix where
 
+import Numeric.Natural
+
 data Associativity = LeftA | RightA | NonA
   deriving (Eq, Show)
 
 data Fixity = Prefix | Postfix | Infix Associativity | Closed
+  deriving (Eq, Show)
+
+data Operator = Operator { fixity :: Fixity, arity :: Natural, nameParts :: [String] }
   deriving (Eq, Show)
