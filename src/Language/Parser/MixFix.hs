eeedcadded2179770f2c8512aac8227479c86475
Rob Rix <rob.rix@github.com>
2016-01-12 21:26:11 -0500

    Fixity.

--- src/Language/Parser/MixFix.hs
+++ src/Language/Parser/MixFix.hs
@@ -2,3 +2,6 @@ module Language.Parser.MixFix where
 
 data Associativity = LeftA | RightA | NonA
   deriving (Eq, Show)
+
+data Fixity = Prefix | Postfix | Infix Associativity | Closed
+  deriving (Eq, Show)
